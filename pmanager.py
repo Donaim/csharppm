7c241fd88fa1de00170e80c989282f938ecc0e4a
d0naim <d0naim@jerrs.dsnet.umcs.pl>
2018-01-22 14:33:45 +0100

    Createg pmanager

--- pmanager.py
+++ pmanager.py
@@ -1,3 +1,35 @@
 import mxml
+import json
 
-# class project(object):
+class project:
+    def __init__(self, file):
+        self.file = file
+        self.dict = mxml.read_xml(self.file)
+
+    def save(self):
+        mxml.write_xml(self.file, self.dict, pretty=True)
+    def print(self):
+        print(mxml.print_dict(self.dict))
+    def print(self, *path):
+        print(self.__getp(path))
+
+    def __get(self, path):
+        re = self.dict
+        for i in path:
+            re = re[i]
+        return re
+    def get(self, *path):
+        return self.__get(path)
+    def getp(self, *path):
+        return self.__getp(path)
+    def __getp(self, path):
+        re = self.__get(path)
+        return json.dumps(re, sort_keys=True, indent=4)
+    
+    def __set(self, val, path):
+        re = self.dict
+        for i in path[0:-1]:
+            re = re[i]
+        re[path[-1]] = val
+    def set(self, val, *path):
+        self.__set(val, path)
--- test.py
+++ test.py
@@ -1,9 +1,22 @@
 import mxml
-import pmanager
+import pmanager as pm
 
-x = mxml.read_xml("shablon.csproj")
-props = x['Project']['PropertyGroup'][0]
-props['AssemblyName'] = 'LUL'
-# mxml.print_dict(props)
+def test_mxml():
+    x = mxml.read_xml("tmp.csproj")
+    props = x['Project']['PropertyGroup'][0]
+    props['AssemblyName'] = 'LUL'
+    # mxml.print_dict(props)
 
-mxml.write_xml('out.csproj', x)
+    mxml.write_xml('out.csproj', x)
+
+def test_pmanager():
+    p = pm.project('tmp.csproj')
+    p.print('Project', 'PropertyGroup', 0)
+    p.set('KEK1', 'Project', 'PropertyGroup', 0, 'AssemblyName')
+    p.print('Project', 'PropertyGroup', 0)
+    p.save()
+
+# make a copy
+mxml.write_file('tmp.csproj', mxml.read_file('shablon.csproj'))
+
+test_pmanager()
