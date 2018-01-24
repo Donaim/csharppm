f30f61a016ee27bbf08004f9abda0e22031e8c5c
d0naim <d0naim@jerrs.dsnet.umcs.pl>
2018-01-24 17:03:55 +0100

    Creating project works

--- csharppm/__init__.py
+++ csharppm/__init__.py
@@ -21,45 +21,45 @@ class MParser(object):
     def __init__(self):
         self.get_objects()
 
-        self.parser = argparse.ArgumentParser(description='C# project manager', usage=slndata.stript_usage_info)
         self.parse_command()
         
     def parse_command(self):
-        self.parser.add_argument('command', help='Solution command {} or project name {}'.format(list(self.sln_actions.keys()), self.projects))
-        args = self.parser.parse_args(sys.argv[1:2])
+        parser = argparse.ArgumentParser(description='C# project manager', usage=slndata.stript_usage_info)
+        parser.add_argument('command', help='Solution command {} or project name {}'.format(list(self.sln_actions.keys()), self.get_project_names()))
+        args = parser.parse_args(sys.argv[1:2])
         for k, v in self.sln_actions.items():
             if(k == args.command): 
                 v()
                 return
-        for p in self.projects:
+        for p in self.get_project_names():
             if(p == args.command):
                 self.parse_project(p)
                 return
         
-        raise Exception("Wrong command \"{}\" expected to be: [{}]".format(args.command, self.projects + list(self.sln_actions.keys())))
+        raise Exception("Wrong command \"{}\" expected to be: {}".format(args.command, self.get_project_names() + list(self.sln_actions.keys())))
 
+    def get_project_names(self):
+        if(self.sln_file != None):
+            return self.sln_mgr.get_project_names()
+        else: return []
     def parse_project(self, project_name):
         print("Parsing {} project ".format(project_name))
 
     def get_objects(self):
         self.sln_file = helper.get_sln_file()
-        if self.sln_file != None: self.sln_name = os.path.basename(self.sln_file).split('.')[0]
-        else: self.sln_name = ""
-
-        self.projects = [] # they will initialize later
+        self.init_solution()
+        
         self.sln_actions = {
             'init' : self.init_solution,
             'addproj': self.add_proj, 
             'backup': self.backup}
 
-        if self.sln_file != None:        
-            self.init_solution()
-
-    def init_solution(self):
-        self.sln_file = props.pjoin(props.slndir, os.path.basename(props.slndir) + '.sln')
+    def init_solution(self): 
+        if self.sln_file == None: 
+            self.sln_file = props.pjoin(props.slndir, os.path.basename(props.slndir) + '.sln') # creates new solution file
+        self.sln_name = os.path.basename(self.sln_file).split('.')[0]
         self.sln_mgr = slnmanager.slnmng(self.sln_file)
-        self.projects = self.sln_mgr.get_project_names()
-
+    
     def backup(self):
         shutil.copytree(props.slndir, props.slndir + '.backup')
         print("{} was backed up successfuly".format(self.sln_name))
@@ -68,25 +68,18 @@ class MParser(object):
         if(self.sln_file == None): 
             self.init_solution()
             # raise Exception("Create solution first with \"{} init\"".format(props.script_name))
-        print("adding project to {}".format(self.sln_name))
+        
+        parser = argparse.ArgumentParser(description='Gets project arguments')
+        parser.add_argument('file')
+        parser.add_argument('--type', default=slndata.csharpstdtype)
+        parser.add_argument('--fver', default=slndata.csharpstdver)
 
-    def commit(self):
-        parser = argparse.ArgumentParser(
-            description='Record changes to the repository')
-        # prefixing the argument with -- means it's optional
-        parser.add_argument('--amend', action='store_true')
-        # now that we're inside a subcommand, ignore the first
-        # TWO argvs, ie the command (git) and the subcommand (commit)
         args = parser.parse_args(sys.argv[2:])
-        print('Running git commit, amend=%s' % args.amend)
+        if ('/' not in args.file) and ('\\' not in args.file): # mean we entered only a name of project, not path
+            args.file = props.pjoin(args.file, args.file + '.csproj')   
 
-    def fetch(self):
-        parser = argparse.ArgumentParser(
-            description='Download objects and refs from another repository')
-        # NOT prefixing the argument with -- means it's not optional
-        parser.add_argument('repository')
-        args = parser.parse_args(sys.argv[2:])
-        print('Running git fetch, repository=%s' % args.repository)
+        self.sln_mgr.create_project(args.file, args.type, args.fver)
+        print("Project {} added to {} solution".format(args.file, self.sln_name))
 
 
 if __name__ == "__main__":
